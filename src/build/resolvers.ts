ba1139e47631da261a7586f91307f611dd7a9950
Ian K Smith <smithki707@gmail.com>
2022-03-08 10:56:51 -0700

    Update resolvers

--- src/build/resolvers.ts
+++ src/build/resolvers.ts
@@ -4,19 +4,41 @@ import { getPackageJson } from '../utils/get-package-json';
 import { getProjectRoot } from '../utils/get-project-root';
 import { BuildContext } from './types';
 
+const entrypointCache = new Map<string, string>();
+
 /**
  * Resolves the entrypoint file for ESBuild,
  * based on the format and target platform.
  */
-export async function resolveEntrypoint(ctx: BuildContext) {
-  const findEntrypoint = async (indexTarget?: string) => {
-    if (ctx.format && (await checkFileExists(`${ctx.srcdir}/index.${indexTarget}.ts`))) {
-      return `${ctx.srcdir}/index.${indexTarget}.ts`;
-    }
-    return `${ctx.srcdir}/index.ts`;
+
+export async function resolveEntrypoint(ctx: BuildContext, options?: { exts?: string[] }): Promise<string> {
+  const { exts = ['ts', 'tsx'] } = options ?? {};
+  const key = `${ctx.format}:${exts.join(',')}`;
+
+  if (entrypointCache.has(key)) {
+    return entrypointCache.get(key)!;
+  }
+
+  const checkExists = async (filepath: string) => {
+    return (await checkFileExists(filepath)) && filepath;
   };
 
-  return findEntrypoint(ctx.format);
+  const checks = await Promise.all([
+    // Check if `index.{format}.{ext}` exists...
+    ...exts.map(async (ext) => checkExists(`${ctx.srcdir}/index.${ctx.format}.${ext}`)),
+
+    // Then fallback to `index.{ext}` if it doesn't...
+    ...exts.map(async (ext) => checkExists(`${ctx.srcdir}/index.${ext}`)),
+  ]);
+
+  const entrypoint = checks.find(Boolean);
+
+  if (!entrypoint) {
+    throw new Error('Could not resolve entrypoint.');
+  }
+
+  entrypointCache.set(key, entrypoint);
+  return entrypoint;
 }
 
 /**
